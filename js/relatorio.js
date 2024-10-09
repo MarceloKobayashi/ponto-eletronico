68db44a1c16c0be99541b0fcacf3e9ff923c897f
MarceloKobayashi <marcelohk2004@gmail.com>
2024-10-08 21:03:01 -0300

    1008 - parte do relatorio, falta filtrar

--- js/relatorio.js
+++ js/relatorio.js
@@ -1,19 +1,64 @@
 function renderList() {
-    registers = JSON.parse(localStorage.getItem("register"));
+
+    const registers = JSON.parse(localStorage.getItem("register")) || [];
 
     const containerRegisters = document.getElementById("registros-relatorio");
 
-    registers.forEach(register => {
-        console.log(register);
+    const tipoAbreviado = {
+        "entrada": "E",
+        "intervalo": "I",
+        "volta-intervalo": "V",
+        "saida": "S"
+    }
 
-        const divRegistro = document.createElement("div");
+    if (containerRegisters) {
+        containerRegisters.innerHTML='';
 
-        divRegistro.innerHTML = `<p>${register.data}</p>`;
+        const grupoPorData = {};
+        
+        registers.forEach(register => {
+            const data = register.data;
+            if (!grupoPorData[data]) {
+                grupoPorData[data] = [];
+            }
+            grupoPorData[data].push(register);
+        });
 
-        //data x date
+        for (const data in grupoPorData) {
+            const registrosPorData = grupoPorData[data];
 
-        containerRegisters.appendChild(divRegistro);
-    })
+            const divData = document.createElement("div");
+            divData.innerHTML = `<p>> ${data}: </p>`;
+            containerRegisters.appendChild(divData);
+
+            registrosPorData.forEach(register => {         
+                const divRegistro = document.createElement("div");
+
+                if (register.observação === "") {
+                    register.observação = "Sem observação.";
+                }
+
+                const tipoAbrev = tipoAbreviado[register.tipo];
+
+                const dadosRegistro = `
+                    <p style="display: flex; align-items: center;">
+                        <span style="width: 10%; padding-right: 5px;">${tipoAbrev}</span>
+                        <span style="width: 15%; padding-right: 5px;">| ${register.hora}</span>
+                        <span style="width: 30%; padding-right: 5px;">| ${register.observação || 'Sem observação'}</span>
+                        <span style="width: 30%;">| ${register.arquivo || 'Sem anexo'}</span>
+                    </p>
+                `;
+                divRegistro.innerHTML = dadosRegistro;
+                    
+                    //data x date
+                    
+                containerRegisters.appendChild(divRegistro);
+            });
+        }
+        
+    } else {
+        console.error("O container de registros não foi encontrado.");
+    }
 }
 
 renderList();
