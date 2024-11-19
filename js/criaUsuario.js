e3cc9f2e1d99256b4832e00c7820cffca7831b8c
MarceloKobayashi <marcelohk2004@gmail.com>
2024-11-19 09:33:42 -0300

    1911 - Acabar em casa

--- js/criaUsuario.js
+++ js/criaUsuario.js
@@ -1,7 +1,32 @@
 document.getElementById("btn-criar-usuario").addEventListener("click", async () => {
-    
-    const response = await fetch("http://localhost:3000/usuarios");
 
-    console.log(response);
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const login = document.getElementById("login").value;
+    const senha = document.getElementById("senha").value;
+    const permissao = document.getElementById("tipo").value;
+    
+    const response = await fetch("http://localhost:3000/usuarios", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+            login: login,
+            senha: senha,
+            permissao: permissao
+        })
+    })
+    .then()
+    .catch(error => {
+        console.log(error);
+    });
+
+    console.log(response.json);
 
 });
+
+//belongs hasmanyusers
+//pesquisar como relacionar duas tabelas 1:N
